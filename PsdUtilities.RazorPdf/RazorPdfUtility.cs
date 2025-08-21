b9da0274be415eeb07a2708b632a31ce0e1bd488
psdlex <faceitlolkekror0@gmail.com>
2025-08-21 22:11:40 +0300

    utility

---
+++ PsdUtilities.RazorPdf/RazorPdfUtility.cs
@@ -0,0 +1,9 @@
+﻿namespace PsdUtilities.RazorPdf;
+
+public static class RazorPdfUtility
+{
+    public static void InstallRequiredDependencies()
+    {
+        Microsoft.Playwright.Program.Main(["Install"]);
+    }
+}
